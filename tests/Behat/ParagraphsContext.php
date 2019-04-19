f9e4ee8fc12c1445439e0279a4b472f9eeee5087
Francesco Sardara <francesco.sardara@ausy.be>
2019-04-19 16:17:01 +0200

    OPENEUROPA-1843: Add a context to deal with paragraph forms.

---
+++ tests/Behat/ParagraphsContext.php
@@ -0,0 +1,277 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_paragraphs\Behat;
+
+use Behat\Mink\Element\NodeElement;
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+
+/**
+ * Provides steps definitions to interact with paragraphs.
+ */
+class ParagraphsContext extends RawDrupalContext {
+
+  /**
+   * Fills a field of a specific paragraph.
+   *
+   * Only fields that directly belong to the paragraph are targeted.
+   *
+   * @param string $field
+   *   The field label.
+   * @param string $value
+   *   The field value.
+   * @param string $paragraph_type
+   *   The label of the paragraph type.
+   * @param string $position
+   *   The ordinal position of the paragraph amongst its type.
+   *
+   * @When I fill in :field with :value in the :position :paragraph_type( paragraph)
+   */
+  public function fillParagraphField(string $field, string $value, string $paragraph_type, string $position): void {
+    $paragraph_type = $this->unescapeStepArgument($paragraph_type);
+    $position = $this->convertOrdinalToNumber($position) - 1;
+
+    $paragraph = $this->findParagraph($paragraph_type, $position);
+    $collection = $this->getParagraphFormFieldCollection($paragraph);
+
+    $collection->fillField($this->unescapeStepArgument($field), $this->unescapeStepArgument($value));
+  }
+
+  /**
+   * Press a button of a specific paragraph.
+   *
+   * Only buttons that directly belong to the paragraph are targeted.
+   *
+   * @param string $button
+   *   The button selector.
+   * @param string $paragraph_type
+   *   The label of the paragraph type.
+   * @param string $position
+   *   The ordinal position of the paragraph amongst its type.
+   *
+   * @When I press :button in the :position :paragraph_type( paragraph)
+   */
+  public function pressParagraphButton(string $button, string $paragraph_type, string $position): void {
+    $paragraph_type = $this->unescapeStepArgument($paragraph_type);
+    $position = $this->convertOrdinalToNumber($position) - 1;
+
+    $paragraph = $this->findParagraph($paragraph_type, $position);
+    $collection = $this->getParagraphFormFieldCollection($paragraph);
+
+    $collection->pressButton($this->unescapeStepArgument($button));
+  }
+
+  /**
+   * Press a button in a specific paragraph actions.
+   *
+   * @param string $button
+   *   The button selector.
+   * @param string $paragraph_type
+   *   The label of the paragraph type.
+   * @param string $position
+   *   The ordinal position of the paragraph amongst its type.
+   *
+   * @throws \Exception
+   *   Thrown when no actions are found for the given paragraph.
+   *
+   * @When I press :button in the :position :paragraph_type (paragraph )actions
+   */
+  public function pressParagraphActionsButton(string $button, string $paragraph_type, string $position): void {
+    $paragraph_type = $this->unescapeStepArgument($paragraph_type);
+    $position = $this->convertOrdinalToNumber($position) - 1;
+
+    $paragraph = $this->findParagraph($paragraph_type, $position);
+    $actions = $paragraph->find('css', 'div.paragraph-top div.paragraphs-actions');
+
+    if (!$actions) {
+      throw new \Exception(sprintf('Could not find actions for "%s" in position "%s".', $paragraph_type, $position));
+    }
+
+    $actions->pressButton($this->unescapeStepArgument($button));
+  }
+
+  /**
+   * Selects an option in a select field of a specific paragraph.
+   *
+   * Only selects that directly belong to the paragraph are targeted.
+   *
+   * @param string $select
+   *   The select field.
+   * @param string $option
+   *   The option value.
+   * @param string $paragraph_type
+   *   The label of the paragraph type.
+   * @param string $position
+   *   The ordinal position of the paragraph amongst its type.
+   *
+   * @When I select :option from :select in the :position :paragraph_type( paragraph)
+   */
+  public function selectOptionInParagraph(string $select, string $option, string $paragraph_type, string $position): void {
+    $paragraph_type = $this->unescapeStepArgument($paragraph_type);
+    $position = $this->convertOrdinalToNumber($position) - 1;
+
+    $paragraph = $this->findParagraph($paragraph_type, $position);
+    $collection = $this->getParagraphFormFieldCollection($paragraph);
+
+    $collection->selectFieldOption($this->unescapeStepArgument($select), $this->unescapeStepArgument($option));
+  }
+
+  /**
+   * Asserts that a specific option is selected in a paragraph select field.
+   *
+   * Only selects that directly belong to the paragraph are targeted.
+   *
+   * @param string $select
+   *   The select field.
+   * @param string $option
+   *   The option value.
+   * @param string $paragraph_type
+   *   The label of the paragraph type.
+   * @param string $position
+   *   The ordinal position of the paragraph amongst its type.
+   *
+   * @throws \Exception
+   *   Thrown when the select cannot be found, when no options or the wrong one
+   *   selected is.
+   *
+   * @Then the option :option should be selected in the :select select of the :position :paragraph_type( paragraph)
+   */
+  public function assertSelectedOptionInParagraph(string $select, string $option, string $paragraph_type, string $position): void {
+    $paragraph_type = $this->unescapeStepArgument($paragraph_type);
+    $position = $this->convertOrdinalToNumber($position) - 1;
+
+    $paragraph = $this->findParagraph($paragraph_type, $position);
+    $collection = $this->getParagraphFormFieldCollection($paragraph);
+
+    $element = $collection->find('named', ['select', $this->unescapeStepArgument($select)]);
+
+    if (empty($element)) {
+      throw new \Exception(sprintf('Could not find select "%s".', $select));
+    }
+
+    $option_element = $element->find('xpath', '//option[@selected="selected"]');
+    if (!$option_element) {
+      throw new \Exception(sprintf('No option is selected for the "%s" select.', $select));
+    }
+
+    if ($option_element->getText() !== $option) {
+      throw new \Exception(sprintf('The option "%s" was not selected, "%s" was selected.', $option, $option_element->getHtml()));
+    }
+  }
+
+  /**
+   * Returns a collection of fields that belong directly to a paragraph.
+   *
+   * @param string $paragraph_type
+   *   The label of the paragraph type.
+   * @param int $position
+   *   The position of the paragraph amongst its type, 0-index based.
+   *
+   * @return \Behat\Mink\Element\NodeElement
+   *   A node element whose XPath targets only the direct fields of the
+   *   paragraph.
+   *
+   * @throws \Exception
+   *   Thrown when the paragraph is not found.
+   */
+  protected function findParagraph(string $paragraph_type, int $position): NodeElement {
+    // Find all paragraphs of the specified type, using the displayed label.
+    $xpath = '//span' . $this->xpathHasClassSelector('paragraph-type-label') . '[text()="' . $paragraph_type . '"]'
+      // Find the closest "top" wrapper.
+      . '/ancestor-or-self::div' . $this->xpathHasClassSelector('paragraph-top')
+      // The parent is the paragraph wrapper.
+      . '/..';
+
+    $paragraphs_of_type = $this->getSession()->getPage()->findAll('xpath', $xpath);
+
+    if (!isset($paragraphs_of_type[$position])) {
+      throw new \Exception(sprintf('Cannot find "%s" in position "%s".', $paragraph_type, $position));
+    }
+
+    return $paragraphs_of_type[$position];
+  }
+
+  /**
+   * Returns a collection of fields that belong directly to a paragraph.
+   *
+   * @param \Behat\Mink\Element\NodeElement $paragraph
+   *   The paragraph element.
+   *
+   * @return \Behat\Mink\Element\NodeElement
+   *   A node element whose XPath targets only the direct fields of the
+   *   paragraph.
+   */
+  protected function getParagraphFormFieldCollection(NodeElement $paragraph): NodeElement {
+    $direct_fields = $paragraph->getXpath()
+      // Find the paragraph subform wrapper which contains the fields.
+      . '/div' . $this->xpathHasClassSelector('paragraphs-subform')
+      // Exclude all the sub-paragraph forms that might be there.
+      . '/*[not(' . $this->xpathHasClassSelector('field--type-entity-reference-revisions', FALSE) . ')]';
+
+    // Include the variant select which is outside the form display.
+    $variant_field = $paragraph->getXpath()
+      . '/div' . $this->xpathHasClassSelector('form-item') . '[.//label[text()="Variant"]]';
+
+    // Create a node element which is actually pointing to a collection of
+    // elements minus the sub-paragraphs.
+    $collection = new NodeElement($direct_fields . ' | ' . $variant_field, $this->getSession());
+
+    return $collection;
+  }
+
+  /**
+   * Creates an XPath selector to match by class.
+   *
+   * @param string $class
+   *   The class.
+   * @param bool $wrap
+   *   When true, wraps the expression between square brackets to be used
+   *   directly as selector. Defaults to true.
+   *
+   * @return string
+   *   The xpath selector.
+   */
+  protected function xpathHasClassSelector(string $class, bool $wrap = TRUE): string {
+    $exp = '@class and contains(concat(" ", normalize-space(@class), " "), " ' . $class . ' ")';
+    return $wrap ? "[{$exp}]" : $exp;
+  }
+
+  /**
+   * Converts an ordinal number to its integer value.
+   *
+   * E.g.: converts 1st to 1, 7th to 7.
+   *
+   * @param string $ordinal
+   *   The ordinal string.
+   *
+   * @return int
+   *   The integer value.
+   *
+   * @throws \Exception
+   *   Thrown when an integer portion cannot be extracted.
+   */
+  protected function convertOrdinalToNumber(string $ordinal): int {
+    preg_match('/^(\d+)(st|nd|rd|th)$/i', $ordinal, $matches);
+
+    if (!isset($matches[1])) {
+      throw new \Exception("Could not extract a number from '$ordinal'.");
+    }
+
+    return (int) $matches[1];
+  }
+
+  /**
+   * Unescapes step arguments.
+   *
+   * @param string $argument
+   *   The argument value.
+   *
+   * @return string
+   *   The unescaped value.
+   */
+  protected function unescapeStepArgument(string $argument): string {
+    return str_replace('\\"', '"', $argument);
+  }
+
+}
