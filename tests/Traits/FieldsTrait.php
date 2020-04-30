1c1bac9a91cea8590e55a1bcc6404a4cf623e054
Francesco Sardara <francesco.sardara@ausy.be>
2020-04-30 23:31:30 +0200

    OPENEUROPA-3149: Add a trait to deal with fields.

---
+++ tests/Traits/FieldsTrait.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_paragraphs\Traits;
+
+use Behat\Mink\Element\NodeElement;
+
+/**
+ * Helper methods to deal with fields.
+ */
+trait FieldsTrait {
+
+  /**
+   * Returns the n-th occurrence of a field in a page.
+   *
+   * @param string $field
+   *   The field element label.
+   * @param int $position
+   *   The position of the field amongst the ones with same label. Zero-based
+   *   index.
+   *
+   * @return \Behat\Mink\Element\NodeElement
+   *   The field element.
+   *
+   * @throws \Exception
+   *   Thrown when the specified occurrence of the field or the field itself is
+   *   not found.
+   */
+  protected function getNthField(string $field, int $position): NodeElement {
+    $fields = $this->getSession()->getPage()->findAll('named', ['field', $field]);
+
+    if (!$fields || !isset($fields[$position])) {
+      throw new \Exception(sprintf('Could not find field "%s" in position "%s".', $field, $position));
+    }
+
+    return $fields[$position];
+  }
+
+  /**
+   * Returns the table of a multiple cardinality field given its label.
+   *
+   * @param string $field
+   *   The field label.
+   * @param int $position
+   *   The position of the field amongst the ones with same label. Zero-based
+   *   index. Defaults to 0 (first occurrence). Optional.
+   *
+   * @return \Behat\Mink\Element\NodeElement
+   *   The table element.
+   *
+   * @throws \Exception
+   *   Thrown when no field table is found.
+   */
+  protected function getMultipleCardinalityFieldTable(string $field, int $position = 0): NodeElement {
+    // This method requires the invoking class to include the UtilityTrait.
+    assert(method_exists($this, 'xpathHasClassSelector'), sprintf('The calling class must use the %s trait.', UtilityTrait::class));
+
+    // Find the table by its header.
+    $xpath = '//table' . $this->xpathHasClassSelector('field-multiple-table')
+      . '[./thead//h4[text()="' . $field . '"]]';
+    $tables = $this->getSession()->getPage()->findAll('xpath', $xpath);
+
+    if (empty($tables) || !isset($tables[$position])) {
+      throw new \Exception(sprintf('Could not find field table "%s" in position "%s".', $field, $position));
+    }
+
+    return $tables[$position];
+  }
+
+}
