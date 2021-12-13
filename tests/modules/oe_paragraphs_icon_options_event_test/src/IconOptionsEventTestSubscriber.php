c21d5a446412c80caa336a25eb9db9ef83446850
Abel Santos <abel.santos.corral@gmail.com>
2021-12-13 12:42:11 +0100

    OEL-759: Eliminate an unused class.

--- tests/modules/oe_paragraphs_icon_options_event_test/src/IconOptionsEventTestSubscriber.php
+++ tests/modules/oe_paragraphs_icon_options_event_test/src/IconOptionsEventTestSubscriber.php
@@ -1,9 +1,10 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace Drupal\oe_paragraphs_icon_options_event_test;
 
 use Drupal\oe_paragraphs\Event\IconOptionsEvent;
-use Drupal\cas\Service\CasHelper;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -14,7 +15,7 @@ class IconOptionsEventTestSubscriber implements EventSubscriberInterface {
   /**
    * {@inheritdoc}
    */
-  public static function getSubscribedEvents() {
+  public static function getSubscribedEvents(): array {
     $events[IconOptionsEvent::class][] = ['getIconOptions', 0];
     return $events;
   }
@@ -25,7 +26,7 @@ class IconOptionsEventTestSubscriber implements EventSubscriberInterface {
    * @param \Drupal\custom_events\Event\IconOptionsEvent $event
    *   Allowed format event object.
    */
-  public function getIconOptions(IconOptionsEvent $event) {
+  public function getIconOptions(IconOptionsEvent $event): void {
     $event->setIconOptions([
       'item-test-1' => 'Item test 1',
       'item-test-2' => 'Item test 1',
