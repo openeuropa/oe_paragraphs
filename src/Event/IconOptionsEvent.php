6e9a8308efc6d9cf08f062ccf2afd87118eada17
Abel Santos <abel.santos.corral@gmail.com>
2021-12-14 13:28:02 +0100

    OEL-759: Fix descriptions at IconOptionsEvent.

--- src/Event/IconOptionsEvent.php
+++ src/Event/IconOptionsEvent.php
@@ -7,7 +7,7 @@ namespace Drupal\oe_paragraphs\Event;
 use Symfony\Component\EventDispatcher\Event;
 
 /**
- * Event triggered when an icon options needs to be provided.
+ * Event triggered when an icon's options need to be provided.
  */
 class IconOptionsEvent extends Event {
 
@@ -19,7 +19,7 @@ class IconOptionsEvent extends Event {
   protected $iconOptions = [];
 
   /**
-   * Sets icon options list.
+   * Sets the icon options list.
    *
    * @param array $icon_options
    *   Array containing the set of allowed values for icon options.
