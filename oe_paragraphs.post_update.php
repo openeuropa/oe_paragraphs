d9707a98912523f369213c21789fec8926b711fe
22Alexandra <beldean.ale@gmail.com>
2024-04-08 09:02:27 +0300

    EWPP-4011: Update Twitter label to X.

--- oe_paragraphs.post_update.php
+++ oe_paragraphs.post_update.php
@@ -246,3 +246,20 @@ function oe_paragraphs_post_update_10009(): void {
   $field_storage->set('settings', $settings);
   $field_storage->save();
 }
+
+/**
+ * Update Twitter label to X in social media links field.
+ */
+function oe_paragraphs_post_update_10010() {
+  $field_storage = \Drupal::entityTypeManager()->getStorage('field_storage_config')->load('paragraph.field_oe_social_media_links');
+  $settings = $field_storage->get('settings');
+  if (!isset($settings['allowed_values']['twitter'])) {
+    return 'The field storage does not contain the twitter key.';
+  }
+  if ($settings['allowed_values']['twitter'] !== 'Twitter') {
+    return 'The label of the twitter key is different than the original value. No update required.';
+  }
+  $settings['allowed_values']['twitter'] = 'X';
+  $field_storage->set('settings', $settings);
+  $field_storage->save();
+}
